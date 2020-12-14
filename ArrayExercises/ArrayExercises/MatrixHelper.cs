324abedd7684b905f1286eac642a017d1fed4eae
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-12-14 20:42:50 +0200

    Read and print matrices

---
+++ ArrayExercises/ArrayExercises/MatrixHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayExercises
+{
+    public static class MatrixHelper
+    {
+        public static int[,] ReadMatrix()
+        {
+            int rows = Utils.ReadNumber("Number of rows=");
+            int cols = Utils.ReadNumber("Number of cols=");
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = Utils.ReadNumber($"elem[{i},{j}]=");
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void Print(string label, int[,] matrix)
+        {
+            if (matrix is null)
+            {
+                return;
+            }
+
+            int consoleWidth = Console.WindowWidth;
+
+            Console.WriteLine(new string('-', consoleWidth - 1));
+            Console.WriteLine(label);
+            Console.WriteLine(new string('-', consoleWidth - 1));
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j], 6}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ArrayExercises/ArrayExercises/Program.cs
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -6,33 +6,8 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            int[] array = { 7, 13, 22, 1, 5 };
-
-            /*
-            index = 0,
-            (1 => array.Length) swap index 0 with 3: 1, 13, 22, 7, 5
-
-            index = 1,
-            (2 => array.Length) swap index 1 with 4: 1, 5, 22, 7, 13
-
-            index = 2,
-            (3 => array.Length) swap index 2 with 3: 1, 5, 7, 22, 13
-
-            index = 3,
-            (4 => array.Length) swap index  with : 1, 5, 7, 13, 22
-
-            index 4 
-            */
-
-            // int[] sortedArrayAsc = ArrayHelper.SelectionSort(array, SortDirection.Ascending);
-            // int[] sortedArrayDesc = ArrayHelper.SelectionSort(array, SortDirection.Descending);
-            int[] fibo = ArrayHelper.Fibonacci(12);
-            ArrayHelper.Print("Fibonacci", fibo);
-
-            // ArrayHelper.Print("Bubble Sort (desc)", sortedArrayDesc);
-
-            int[] primes = ArrayHelper.EratostenePrimes(120);
-            ArrayHelper.Print("Prime numbers", primes);
+            int[,] matrix = MatrixHelper.ReadMatrix();
+            MatrixHelper.Print("Matrix:", matrix);
         }
     }
 }
